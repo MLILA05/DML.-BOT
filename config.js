e9e4ea6e7ee54cc80a9e89e156cf07c004c50d2c
MLILA05 <dmlemegency1234@gmail.com>
2025-12-10 17:57:49 +0300

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "DML-MD~TIZS1bRB#tdEuL2kc47iAhItPKKQ_XaqkKLXp8zcqfR9_BoFxsQ8",
+SESSION_ID: process.env.SESSION_ID || "DML-MD",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
@@ -13,7 +13,7 @@ AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
 // make true if you want auto reply on status 
 AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
 // make true if you want auto reply on status 
-AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS BY DML-MD*",
+AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS BY DML-TECH 🇹🇿*",
 // set the auto reply massage on status reply  
 WELCOME: process.env.WELCOME || "true",
 // true if want welcome and goodbye msg in groups    
@@ -23,13 +23,13 @@ ANTI_LINK: process.env.ANTI_LINK || "true",
 // make anti link true,false for groups 
 MENTION_REPLY: process.env.MENTION_REPLY || "false",
 // make true if want auto voice reply if someone menetion you 
-MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://url.bwmxmd.online/Adams.t2dpkvu3.jpg",
+MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/z2rr5a.jpg",
 // add custom menu and mention reply image url
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
-BOT_NAME: process.env.BOT_NAME || "DML-MD",
+BOT_NAME: process.env.BOT_NAME || "DML-TECH",
 // add bot namw here for menu
-STICKER_NAME: process.env.STICKER_NAME || "DML-MD",
+STICKER_NAME: process.env.STICKER_NAME || "DML-TECH",
 // type sticker pack name 
 CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
 // make this true for custum emoji react    
@@ -37,15 +37,15 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "255713541112",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "255615752312",
 // add your bot owner number
 OWNER_NAME: process.env.OWNER_NAME || "DML",
 // add bot owner name
-DESCRIPTION: process.env.DESCRIPTION || "*© Powered By Dml*",
+DESCRIPTION: process.env.DESCRIPTION || "*© Powered by DML*",
 // add bot owner name    
-ALIVE_IMG: process.env.ALIVE_IMG || "https://url.bwmxmd.online/Adams.t2dpkvu3.jpg",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/z2rr5a.jpg",
 // add img for alive msg
-LIVE_MSG: process.env.LIVE_MSG || "> AM ACTIVE *DML-MD",
+LIVE_MSG: process.env.LIVE_MSG || "> AM ACTIVE *DML-MD*⚡",
 // add alive msg here 
 READ_MESSAGE: process.env.READ_MESSAGE || "false",
 // Turn true or false for automatic read msgs
@@ -71,18 +71,17 @@ AUTO_TYPING: process.env.AUTO_TYPING || "false",
 // true for automatic show typing   
 READ_CMD: process.env.READ_CMD || "false",
 // true if want mark commands as read 
-DEV: process.env.DEV || "255713541112",
+DEV: process.env.DEV || "255622220680",
 //replace with your whatsapp number        
 ANTI_VV: process.env.ANTI_VV || "true",
 // true for anti once view 
-ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "log", 
+ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "inbox", 
 // change it to 'same' if you want to resend deleted message in same chat 
 AUTO_RECORDING: process.env.AUTO_RECORDING || "false",
 // make it true for auto recoding
-AUTO_BIO: process.env.AUTO_BIO || "true", 
-// Auto update bio
 ANTICALL: process.env.ANTICALL || "false",
-// make it true for auto recoding 
+// make it true for auto call rejection by dml 
 
+//dml
 };    
     
